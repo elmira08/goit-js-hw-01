3d4852c28c0901c41331f3c7c138633e484038cb
Elmira <xzrklf@gmail.com>
2025-02-10 15:05:49 +0200

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
-    let contentN = Number.parseFloat(content);
-    let paddingN = Number.parseFloat(padding);
-    let borderN = Number.parseFloat(border);
-    let width = contentN + paddingN * 2 + borderN * 2;
-    return width;
+    content = Number.parseFloat(content);
+    padding = Number.parseFloat(padding);
+    border = Number.parseFloat(border);
+    return content + padding * 2 + border * 2;
+    
 } 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
